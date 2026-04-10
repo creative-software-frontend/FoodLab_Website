3832c7adaafd0a917e834e2ffde6ef33a50a1f81
samiacodes <emiliyalamia@gmail.com>
2026-04-10 13:30:42 +0600

    Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-amber-800 text-9xl">Lamiya</h1>
+      <Button variant={"ghost"} className="text-5xl bg-amber-700 py-6 px-12" >Fuck Me</Button>
     </div>
   );
 }
