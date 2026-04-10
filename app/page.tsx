ba82a4946ede13d89b393279fc0f90aa38f7524f
samiacodes <emiliyalamia@gmail.com>
2026-04-10 13:30:59 +0600

    Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-amber-800 text-9xl">Lamiya</h1>
-      <Button variant={"ghost"} className="text-5xl bg-amber-700 py-6 px-12" >Fuck Me</Button>
+      <Button variant={"ghost"} className="text-5xl bg-amber-700 py-6 px-12" >Click Me</Button>
     </div>
   );
 }
